f5aba8118b63c7741a4da255911d5ad613be5b60
shihanwj <shihanwei495@gmail.com>
2024-10-03 13:48:47 +0200

    Delete Prestamo directory

--- Prestamo/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module es.ieslosmontecillos.prestamo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens es.ieslosmontecillos.prestamo to javafx.fxml;
-    exports es.ieslosmontecillos.prestamo;
-}
